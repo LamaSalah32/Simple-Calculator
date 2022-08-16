f8c6e0a8468d8158598ec6dc41003fb8d8511fd6
lama salah <lamasalah32@gmail.com>
2022-08-16 11:43:09 +0200

    Update simple_calc.php
    add exceptions message in division case

--- simple_calc.php
+++ simple_calc.php
@@ -27,41 +27,65 @@
 
 <?php
      
+     function division($a, $b){
+            if (!$b){
+                  throw new Exception("Can't divide by 0!");
+            }     
+
+            return $a / $b;
+      }
+
       $s = $_POST["submit"];
       $res = "Result";
-
-      $num1 = $_POST['n1'];
-      $num2 = $_POST['n2'];
+      
+      $num1 = filter_input(INPUT_POST, 'n1', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+      $num2 = filter_input(INPUT_POST, 'n2', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
       
       if ($num1 == NULL || $num2 == NULL){
-            echo "<div> <p class=\"c1\"> <input value=\"\" disabled class=\"res\">  </p> </div>";
+            echo "<div> <p class=\"c1\"> <input style=\"color:gray\" value=\"Enter two numbers\" disabled class=\"res\">  </p> </div>";
       }
       else{
-            switch($s){
-                  case '+':
-                              $res =  $num1 +  $num2;
-                              break;
-                  case '-':
-                              $res = $num1 -  $num2;
-                              break; 
-                  
-                  case '×':
-                              $res = $num1 *  $num2;
-                              break;
+            if (isset($_POST["submit"])){
+                  switch($s){
+                        case '+':
+                                    $res =  $num1 +  $num2;
+                                    break;
+                        case '-':
+                                    $res = $num1 -  $num2;
+                                    break; 
+                        
+                        case '×':
+                                    $res = $num1 *  $num2;
+                                    break;
 
-                  case '÷':
-                              if ($num2 == 0){
-                                    echo "<div> <p class=\"c1\"> <input value=\"Can't divide by 0!\" disabled class=\"res\">  </p> </div>";
-                              }
-                              else {
-                                    $res = $num1/$num2;
-                                    echo "<div> <p class=\"c1\"> <input value=\"$num1 $s $num2 = $res\" disabled class=\"res\">  </p> </div>";
-                              }
-                              break;
+                        case '÷':
+                                    try{
+                                          $res = division($num1, $num2);
+                                          echo "<div> 
+                                                      <p class=\"c1\"> 
+                                                      <input value=\"$num1 $s $num2 = $res\" disabled class=\"res\">  
+                                                      </p> 
+                                                </div>";
+
+                                    }catch(Exception $e){
+                                          $msg = $e->getMessage();
+                                          echo "<div> 
+                                                      <p class=\"c1\">
+                                                      <input value=\"$msg\" disabled class=\"res2\"> 
+                                                      </p>
+                                                </div>";
+                                    }
+                                    break;
+                  }
+
+                  if ($s != '÷'){     
+                        echo  "<div> 
+                                    <p class=\"c1\"> 
+                                    <input value=\"$num1 $s $num2 = $res\" disabled class=\"res\">  
+                                    </p> 
+                              </div>";
+                  }
             }
-
-            if ($s != '÷')      
-            echo "<div> <p class=\"c1\"> <input value=\"$num1 $s $num2 = $res\" disabled class=\"res\">  </p> </div>";
       }
       
 ?>
